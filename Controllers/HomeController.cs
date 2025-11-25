ab83cb603dc528bee76ed04d58d7311405972222
ShriGit01 <shripavanb@gmail.com>
2025-11-25 12:25:08 +0530

    First Major changes to site

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -17,11 +17,19 @@ namespace ShriGo.Controllers
         {
             return View();
         }
-
+        public IActionResult ContactUs()
+        {
+            return View();
+        }
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
         public IActionResult Privacy()
         {
             return View();
         }
+        
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
---
+++ Views/Home/Rideupdate.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ShriGo.Views.Home
+{
+    public class RideupdateModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
